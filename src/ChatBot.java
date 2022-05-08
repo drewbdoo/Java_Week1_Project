a5c520524c4dedce53ee0a0194607a157ba3560e
Drew <34400283+drewbdoo@users.noreply.github.com>
2022-05-08 11:09:17 -0400

    adding repeating name, adding CoffeeBot5000

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 import javax.swing.*;
 import java.awt.*;
@@ -12,12 +13,53 @@ public class ChatBot {
         if (response == JOptionPane.NO_OPTION) {
             System.exit(0);
         }
-        String name = JOptionPane.showInputDialog(null, nameRepeat()); //User's name
+        String firstName = JOptionPane.showInputDialog(null, nameFirst());
+        String name = JOptionPane.showInputDialog(null, nameRepeat(firstName)); //User's name
         String response1 = guessingAge1(name); //Which movie they saw in high school
         String result = guessPres(response1, name); //Result of the age guessing game
         JOptionPane.showMessageDialog(null, result);
         int num = counting();
         programmingTest(name);
+
+        int responseCoff = JOptionPane.showConfirmDialog(null, greetingCoff(), "",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (responseCoff == JOptionPane.YES_NO_OPTION) {
+            System.out.println("\n" +
+                    " $$$$$$\\             $$$$$$\\   $$$$$$\\                             $$$$$$$\\             $$\\     \n" +
+                    "$$  __$$\\           $$  __$$\\ $$  __$$\\                            $$  __$$\\            $$ |    \n" +
+                    "$$ /  \\__| $$$$$$\\  $$ /  \\__|$$ /  \\__|$$$$$$\\   $$$$$$\\          $$ |  $$ | $$$$$$\\ $$$$$$\\   \n" +
+                    "$$ |      $$  __$$\\ $$$$\\     $$$$\\    $$  __$$\\ $$  __$$\\ $$$$$$\\ $$$$$$$\\ |$$  __$$\\\\_$$  _|  \n" +
+                    "$$ |      $$ /  $$ |$$  _|    $$  _|   $$$$$$$$ |$$$$$$$$ |\\______|$$  __$$\\ $$ /  $$ | $$ |    \n" +
+                    "$$ |  $$\\ $$ |  $$ |$$ |      $$ |     $$   ____|$$   ____|        $$ |  $$ |$$ |  $$ | $$ |$$\\ \n" +
+                    "\\$$$$$$  |\\$$$$$$  |$$ |      $$ |     \\$$$$$$$\\ \\$$$$$$$\\         $$$$$$$  |\\$$$$$$  | \\$$$$  |\n" +
+                    " \\______/  \\______/ \\__|      \\__|      \\_______| \\_______|        \\_______/  \\______/   \\____/ \n" +
+                    "                                                                                                \n" +
+                    "                                                                                                \n" +
+                    "                                                                                                \n"
+            );
+
+            System.out.println("Barista Wage Slave, prepare to get to work!!!!" + "\n" + "\n");
+            String coffType = coffeeOrder();
+            String coffeeSize = coffeeSize();
+            String orderName = JOptionPane.showInputDialog(null, orderName());
+            String price = priceCal(coffeeSize);
+
+
+            System.out.println("Wage slave!" + "\n" +
+                    "Chop-chop!"+ "\n" +
+                    name + " has a new order for you!" + "\n" + "\n" +
+                    "They need a " + coffType + "\n" + "\n" +
+                    "and they need it to be a " + coffeeSize + "\n" + "\n" +
+                    "And be quick about it!!!");
+
+            finalTotal(orderName, coffType, coffeeSize, price);
+
+
+
+
+        }
+
+
         story(name, response1, num);
         mondaysMakeMeSad();
     }
@@ -28,12 +70,25 @@ public class ChatBot {
                 + "<html>Your one-stop shop for all your chatting needs. </html>" + "\n" + "\n"
                 + "<html>Ready to start <span style='color:#f542c2'>CHATTING?</span></html>");
     }
+    public static String greetingCoff() {
+        return ("<html>Hello!</html>" + "\n"+ "\n"
+                + "<html>Welcome to <span style='color:#7a2f0c'>Coffee-Bot-5000</span></html>" + "\n" + "\n"
+                + "<html>Your one-stop shop for all your caffiene needs. </html>" + "\n" + "\n"
+                + "<html>Ready to start <span style='color:red'>THE CAFFEINATION PROCESS?</span></html>");
+    }
 
-    public static String nameRepeat() {
+
+    public static String nameFirst() {
         return ("<html>Let's get started then</html>" + "\n" + "\n"
                 + "<html><span style='color:#32CD32'>Chatter-Bot6000</span> needs to know your name to get started</html>" + "\n" +"\n"
                 + "<html>We won't use it for any nefarious purposes... </html>" + "\n"
                 + "<html>probably...</html>");
+
+    }
+    public static String nameRepeat(String name) {
+        return ("Are you totally sure about that?" + "\n" + "\n"
+                + "I mean... " + name + " is, well, frankly kind of weird." + "\n" + "\n"
+                + "<html>This is your last chance to confirm that is really... really what you want me to call you</html>" + "\n");
 
     }
 
@@ -118,9 +173,9 @@ public class ChatBot {
                     "President question", JOptionPane.QUESTION_MESSAGE, null, selectionValues6, initialSelection6);;
                     String response2 = (String.valueOf(selection6));
             if(response2.equals("Bill Clinton")){
-                return ("Your age is approximately <html><span style='color:#d622a3'>21-26 years old</span></html>");
+                return ("<html>Your age is approximately <html><span style='color:#d622a3'>21-26 years old</span></html>");
             } else if(response2.equals("George W Bush")){
-                return ("Your age is approximately <html><span style='color:#d622a3'>13-21 years old</span></html>\"");
+                return ("<html>Your age is approximately <html><span style='color:#d622a3'>13-21 years old</span></html>\"");
             } else {
                 return ("<html>You were a <span style='color:red'>LIAR!!!</span></html>" + "\n"
                 + "<html>You're <span style='color:red'>too young</span> to have been in high school when Jurassic World or Wonder Woman 1984 came out!</html>");
@@ -139,7 +194,7 @@ public class ChatBot {
         String outPut = " ";
          for(int z = 1 ; z<=i; z++ ){
              String s = Integer.toString(z);
-             outPut = outPut + s + " ";
+             outPut = outPut + s + " "+"\n";
            }
          JOptionPane.showMessageDialog(null,"The numbers from 1 to " +i + " are" + "\n" + outPut);
          JOptionPane.showMessageDialog(null,"<html>Wow, wasn't that a thrilling example of what a powerful AI like <span style='color:#00f0d8'>Counter-Bot9000</span> is truly capable of?</html>" + "\n"+ "\n"
@@ -152,9 +207,10 @@ public class ChatBot {
      public static void programmingTest(String name) {
          JOptionPane.showMessageDialog(null, "<Html>What a wonderful display of counting by <span style='color:#00f0d8'>Counter-Bot9000</span></html>" + "\n"
                  + "<html>Let's all give him a round of applause, shall we?</html>" + "\n" + "\n"
-                 + "<html>Well it really has been grand and I hope you've enjoyed talking with your good buddy<span style='color:#32CD32'>Chatter-Bot6000!</span></html>" + "\n"
-                 + "<html>Sadly, all <span style='color:#8800f0'>good things</span> must come to an end, but don't despair! I have a great story for you and some bonus easter egg's about how I feel during the week! </html>" + "\n" + "\n"
-                 + "<html>Unfortunately, I need to test your progamming skills first. Click ok when you're ready to for the test!</html>");
+                 + "<html>Well it really has been grand and I hope you've enjoyed talking with your good buddy <span style='color:#32CD32'>Chatter-Bot6000!</span></html>" + "\n" + "\n"
+                 + "<html>Sadly, all <span style='color:#8800f0'>good things</span> must come to an end, but don't despair!</html>" + "\n" + "\n"
+                 +"<html>I have a great story for you and some bonus easter eggs about how I feel during the week! </html>" + "\n" + "\n"
+                 + "<html>Unfortunately, I need to test your <span style='color:red'>Progamming Skills </span>first. Click ok when you're ready to for the test!</html>");
          boolean loopVar = true;
          boolean loopVar2 = true;
          boolean loopVar3 = true;
@@ -202,8 +258,9 @@ public class ChatBot {
                      "Quiz question 3", JOptionPane.QUESTION_MESSAGE, null, selectionValues3, initialSelection3);
              String answer3 = (String.valueOf(selection3));
              if (answer3.equals("System.out.println(\"Howdy Partner\");")) {
-                 JOptionPane.showMessageDialog(null, "Correct! You aced the test! Now you get all the awesome easter eggs." + "\n"
-                         +"How about a special story, all about you!?!");
+                 JOptionPane.showMessageDialog(null, "Correct! You aced the test! Now you get all the awesome easter eggs." + "\n" + "\n"
+                         +"<html>That was a really tough test - I think you deserve a visit to <span style='color:#7a2f0c'>Coffee-Bot-5000</span> for a nice drink</html>" + "\n" + "\n"
+                         +"Then, when you come back, I'll tell you a special story, all about you!!");
                  loopVar3 = false;
              } else {
                  JOptionPane.showMessageDialog(null, "I'm sorry, that was incorrect, please try again.");
@@ -263,5 +320,118 @@ public class ChatBot {
             }while(loopWeek);
 
         }
+    public static String coffeeOrder() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues = {"Hot Coffees", "Iced Coffees", "Hot Teas", "Iced Teas", "Steamers/Hot Chocolate"};
+        String initialSelection = "Hot Coffees";
+        Object selection = JOptionPane.showInputDialog(null, "<html>Thanks for choosing to make your order with <span style='color:#7a2f0c'>Coffee-Bot-5000</span></html>" + "\n" + "\n" +
+                        "<html><span style='color:#7a2f0c'>Coffee-Bot-5000</span> is ready to serve your coffee needs!!</html>" + "\n" + "\n"
+                        + "Please make your initial selection." + "\n" + "\n",
+                "Menu items", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
+        if(selection.equals("Hot Coffees")) {
+            return hotC();
+        } else if(selection.equals("Iced Coffees")) {
+            return icedC();
+        } else if(selection.equals("Hot Teas")){
+            return hotT();
+        } else if(selection.equals("Iced Teas")){
+            return icedT();
+        } else {
+            return steamers();
+        }
+
+    }
+
+    public static String hotC() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues1 = {"Drip Coffee", "Latte", "Mocha", "Cappuccino", "Cafe au Lait"};
+        String initialSelection1 = "Drip Coffee";
+        Object selection1 = JOptionPane.showInputDialog(null, "<html>Please make your <span style='color:#7a2f0c'>Hot Coffee</span> selection</html>" + "\n" + "\n",
+                "Hot Coffee selection", JOptionPane.QUESTION_MESSAGE, null, selectionValues1, initialSelection1);
+
+        String type = (String.valueOf(selection1));
+        return (type);
+
+    }
+    public static String icedT() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues1 = {"Iced Black Tea", "Iced Green Tea", "Iced Chai", "Iced Mango-Passionfruit Tea"};
+        String initialSelection1 = "Iced Black Tea";
+        Object selection1 = JOptionPane.showInputDialog(null, "<html>Please make your <span style='color:#c4d12e'>Iced Tea</span> selection</html>" + "\n" + "\n",
+                "Iced Tea selection", JOptionPane.QUESTION_MESSAGE, null, selectionValues1, initialSelection1);
+
+        String type = (String.valueOf(selection1));
+        return (type);
+
+    }
+    public static String hotT() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues1 = {"Jasmine Green Tea", "Earl Grey", "Herbal Chamomile"};
+        String initialSelection1 = "Jasmine Green Tea";
+        Object selection1 = JOptionPane.showInputDialog(null, "<html>Please make your <span style='color:#8ad12e'>Hot Tea</span> selection</html>" + "\n" + "\n",
+                "Hot Tea selection", JOptionPane.QUESTION_MESSAGE, null, selectionValues1, initialSelection1);
+
+        String type = (String.valueOf(selection1));
+        return (type);
+
+    }
+    public static String icedC() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues1 = {"Iced Latte", "Iced Mocha", "Iced Vanilla Latte", "Iced Dirty Chai"};
+        String initialSelection1 = "Iced Latte";
+        Object selection1 = JOptionPane.showInputDialog(null, "<html>Please make your <span style='color:#d19b2e'>Iced Coffee</span> selection</html>" + "\n" + "\n",
+                "Iced Coffee selection", JOptionPane.QUESTION_MESSAGE, null, selectionValues1, initialSelection1);
+
+        String type = (String.valueOf(selection1));
+        return (type);
+
+    }
+
+    public static String steamers() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues1 = {"Hot Chocolate", "Strawberry Steamer", "Vanilla Steamer", "Black & White Steamer"};
+        String initialSelection1 = "Hot Chocolate";
+        Object selection1 = JOptionPane.showInputDialog(null, "<html>Please make your <span style='color:##8f9466'>Steamer</span> selection</html>" + "\n" + "\n",
+                "Steamer selection", JOptionPane.QUESTION_MESSAGE, null, selectionValues1, initialSelection1);
+
+        String type = (String.valueOf(selection1));
+        return (type);
+
+    }
+    public static String coffeeSize() {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Object[] selectionValues1 = {"Small (12oz)", "Medium (16oz)", "Large (20oz)"};
+        String initialSelection1 = "Small (12oz)";
+        Object selection1 = JOptionPane.showInputDialog(null, "<html>Please make a selection for your drink <span style='color:##2e41d1'>size</span></html>" + "\n" + "\n",
+                "Drink Size", JOptionPane.QUESTION_MESSAGE, null, selectionValues1, initialSelection1);
+
+        String type = (String.valueOf(selection1));
+        return (type);
+
+    }
+    public static String orderName(){
+        return ("Can I get a name for your order?");
+    }
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    public static String priceCal(String size) {
+
+        double s = 2.99;
+        double m = 3.99;
+        double l = 4.99;
+
+        if(size.equals("Small 12oz")){
+            return ("The price for your drink is $" + s + ", and your total after tax is $" + df.format(s * 1.075));
+        } else if (size.equals("Medium (16oz")) {
+            return ("The price for your drink is $" + m + ", and your total after tax is $" + df.format(m * 1.075));
+        }else {
+            return ("The price for your drink is $" + l + ", and your total after tax is $" + df.format(l * 1.075));
+        }
+    }
+
+    public static void finalTotal(String name, String type, String size, String price) {
+        JOptionPane.showMessageDialog(null, "Thank you for your order " + name + "\n" + "\n" +
+                "The Barista-Wage-Slave on duty will be right with your " + size + " " + type + " and to collect payment" + "\n" + "\n" +
+                price);
+    }
 
 };
